99c644cd354a2e80e0c8ff548eba6fca87fb7822
Herminio Vazquez <canimus@gmail.com>
2017-06-16 23:46:27 +0200

    Reverse to the default CDN

--- main.js
+++ main.js
@@ -158,20 +158,85 @@ Vue.component('qfp-content-title', {
   <div>
     <h1 class="title">Heineken</h1>
     <h2 class="subtitle">Order to Cash / Business Processes</h2>
-    <hr>
   </div>
   `
 })
 
 Vue.component('qfp-content-body', {
   template: `
-  <div>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-    Ut nihil quis ex culpa sit pariatur recusandae excepturi
-    mollitia maiores omnis iste, aut quasi autem,
-    in aliquid deserunt quam quos saepe.
-    </p>
+  <div class="tile is-parent is-paddingless">
+
+    <div class="tile is-parent is-3" style="margin-top: 40px">
+      <article class="tile is-child box">
+        <div class="content">
+          <p class="title">SAP GUI</p>
+          <p class="subtitle">A_01</p>
+          <div class="content">
+            <figure class="image is-4by3">
+              <img src="http://bulma.io/images/placeholders/640x480.png">
+            </figure>
+          </div>
+          <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar.</p>
+          </div>
+        </div>
+      </article>
+    </div>
+
+    <div class="tile is-parent is-3" style="margin-top: 40px">
+      <article class="tile is-child box">
+        <div class="content">
+          <p class="title">Login</p>
+          <p class="subtitle">A_02</p>
+          <div class="content">
+            <figure class="image is-4by3">
+              <img src="http://bulma.io/images/placeholders/640x480.png">
+            </figure>
+          </div>
+          <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar.</p>
+          </div>
+        </div>
+      </article>
+    </div>
+
+
+    <div class="tile is-parent is-3" style="margin-top: 40px">
+      <article class="tile is-child box">
+        <div class="content">
+          <p class="title">New sales order</p>
+          <p class="subtitle">A_03</p>
+          <div class="content">
+            <figure class="image is-4by3">
+              <img src="http://bulma.io/images/placeholders/640x480.png">
+            </figure>
+          </div>
+          <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar.</p>
+          </div>
+        </div>
+      </article>
+    </div>
+
+
+    <div class="tile is-parent is-3" style="margin-top: 40px">
+      <article class="tile is-child box">
+        <div class="content">
+          <p class="title">Procurement</p>
+          <p class="subtitle">A_04</p>
+          <div class="content">
+            <figure class="image is-4by3">
+              <img src="http://bulma.io/images/placeholders/640x480.png">
+            </figure>
+          </div>
+          <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar.</p>
+          </div>
+        </div>
+      </article>
+    </div>
+
+
   </div>
   `
 })
